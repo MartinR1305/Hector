3eefeb3e898c99fb1104afaac568e478b233ef84
Martin Recher <martin.recher@etu.uca.fr>
2024-03-15 11:58:50 +0100

    1er page

--- Hector/FormMain.cs
+++ Hector/FormMain.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Hector
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
+
+            // On définit que le treeView de la partie gauche ne pourra pas faire moins de 200 pixels lors de l'exécution.
+            splitContainer1.Panel1MinSize = 200;
         }
     }
 }
